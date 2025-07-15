7da4f5436ec30171e66af866f4e4008671fb6426
Thom Hos <thom.hos@gmail.com>
2025-07-15 22:59:30 +0900

    fix

--- src/game/update.ts
+++ src/game/update.ts
@@ -1,13 +1,13 @@
 import { GameState, InputState } from '../types';
 import { applySystemAction, applyPlayerAction, mapInputToActions } from '../actions';
-import { pipe } from '../utils';
+import { pipe, when } from '../utils';
 
 export const update = (state: GameState, input: InputState, fixedTimeStep: number): GameState => {
     const actions = mapInputToActions(input, state);
 
     return pipe(
         state,
-        (s) => ({ ...s, gameTime: s.gameMode !== 'paused' ? s.gameTime + fixedTimeStep : s.gameTime }),
+        when(state.gameMode !== 'paused', (s) => ({ ...s, gameTime: s.gameTime + fixedTimeStep })),
         (s) => actions.reduce((acc, action) => applySystemAction(acc, action, fixedTimeStep), s),
         (s) => actions.reduce((acc, action) => applyPlayerAction(acc, action, fixedTimeStep), s)
     );
